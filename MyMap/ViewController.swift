d8307be85a9534ccb82dc653649a9453506cf1be
pco2699 <aenkun@gmail.com>
2017-05-29 21:30:39 +0900

    All

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    InputText.delegate = self
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +23,56 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  @IBOutlet weak var InputText: UITextField!
+  @IBOutlet weak var dispMap: MKMapView!
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // キーボードを閉じる
+    textField.resignFirstResponder()
+    
+    let searchKeyword =  textField.text
+    print(searchKeyword ?? "値が入っていません")
+    
+    let geocoder = CLGeocoder()
+    
+    geocoder.geocodeAddressString(searchKeyword!, completionHandler: { (placemarks:[CLPlacemark]?, error:Error?) in
+      
+      if let placemark = placemarks?[0] {
+        if let targetCoordinate = placemark.location?.coordinate {
+          print(targetCoordinate)
+          
+          let pin = MKPointAnnotation()
+          
+          pin.coordinate = targetCoordinate
+          
+          pin.title = searchKeyword
+          
+          self.dispMap.addAnnotation(pin)
+          
+          self.dispMap.region = MKCoordinateRegionMakeWithDistance(targetCoordinate, 500.0, 500.0)
+        }
+      }
+    })
+    
+    return true
+  }
+  
+  @IBAction func changeMapButtonAction(_ sender: Any) {
+    if dispMap.mapType == .standard {
+      dispMap.mapType = .satellite
+    }
+    else if dispMap.mapType == .satellite {
+      dispMap.mapType = .hybrid
+    }
+    else if dispMap.mapType == .hybrid {
+      dispMap.mapType = .satelliteFlyover
+    }
+    else if dispMap.mapType == .satelliteFlyover {
+      dispMap.mapType = .hybridFlyover
+    }
+    else {
+      dispMap.mapType = .standard
+    }
+  }
 }
 
